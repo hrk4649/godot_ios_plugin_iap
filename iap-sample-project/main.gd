14cf61eadd3325e1d4db5b6a8f26a7573790944e
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2025-04-17 08:31:36 +0900

    get the plugin singleton

--- iap-sample-project/main.gd
+++ iap-sample-project/main.gd
@@ -2,3 +2,13 @@ extends Node2D
 
 func _ready() -> void:
     print("_ready")
+    if !Engine.has_singleton("IOSInAppPurchase"):
+        print("IOSInAppPurchase is not found")
+        return
+
+    print("IOSInAppPurchase is found")
+    var singleton = Engine.get_singleton("IOSInAppPurchase")
+    var data = {
+        "message":"hello"
+    }
+    print(singleton.request("products", data))
