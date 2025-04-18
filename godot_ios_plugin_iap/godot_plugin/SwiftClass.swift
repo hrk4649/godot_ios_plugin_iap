2cc744c567f0c7da22b0dd87ec8bb1bd65603e3c
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2025-04-18 17:29:17 +0900

    add requestPurchase()

--- godot_ios_plugin_iap/godot_plugin/SwiftClass.swift
+++ godot_ios_plugin_iap/godot_plugin/SwiftClass.swift
@@ -19,6 +19,8 @@ import StoreKit
             return requestDummy()
         case "products":
             return requestProducts(data:a2)
+        case "purchase":
+            return requestPurchase(data:a2)
         default:
             return 1
         }
@@ -88,6 +90,87 @@ import StoreKit
                 // send error as signal
                 let errorData = [
                     "request":"products",
+                    "error":error.localizedDescription
+                ]
+                response(a1: "error", a2: errorData)
+            }
+        }
+        return 0
+    }
+    
+    static func requestPurchase(data:NSDictionary) -> Int {
+        print("requestPurchase")
+        if data.object(forKey:"product_id") == nil {
+            print("requestPurchase: no 'product_id'")
+            return 1
+        }
+        let productId = data["product_id"] as? String
+        if productId == nil {
+            print("requestPurchase: product_id is not string")
+            return 1
+        }
+        
+        Task {
+            do {
+                let products = try await Product.products(for:[productId!])
+                print("requestPurchase:products:\(products)")
+                if products.count == 0 {
+                    let errorData = [
+                        "request":"purchase",
+                        "error":"no product_id:\(productId!)"
+                    ]
+                    response(a1: "error", a2: errorData)
+                }
+                let product = products[0]
+                let result:Product.PurchaseResult = try await product.purchase()
+                print("requestPurchase:purchase:\(result)")
+                switch result {
+                case .success(.verified(let transaction)):
+                    await transaction.finish()
+                    let resultData = [
+                        "request":"purchase",
+                        "product_id": productId!,
+                        "result":"success"
+                    ]
+                    response(a1: "purchase", a2: resultData)
+                    break
+                case .success(.unverified(_, let error)):
+                    let resultData = [
+                        "request":"purchase",
+                        "product_id": productId!,
+                        "result":"unverified"
+                    ]
+                    response(a1: "purchase", a2: resultData)
+                    break
+                case .pending:
+                    let resultData = [
+                        "request":"purchase",
+                        "product_id": productId!,
+                        "result":"pending"
+                    ]
+                    response(a1: "purchase", a2: resultData)
+                    break
+                case .userCancelled:
+                    let resultData = [
+                        "request":"purchase",
+                        "product_id": productId!,
+                        "result":"userCancelled"
+                    ]
+                    response(a1: "purchase", a2: resultData)
+                    break
+                @unknown default:
+                    let resultData = [
+                        "request":"purchase",
+                        "product_id": productId!,
+                        "result":"unknown"
+                    ]
+                    response(a1: "purchase", a2: resultData)
+                    break
+                }
+            } catch {
+                // send error as signal
+                let errorData = [
+                    "request":"purchase",
                     "error":error.localizedDescription
                 ]
                 response(a1: "error", a2: errorData)
