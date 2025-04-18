a2a474279117efcaa71615c2e1a0ccced8bce61a
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2025-04-19 08:28:27 +0900

    display purchased items

--- iap-sample-project/main.gd
+++ iap-sample-project/main.gd
@@ -1,58 +1,73 @@
 extends Control
 
 var ItemButton = preload("res://item_button.tscn")
+var ItemLabel = preload("res://item_label.tscn")
 
-@onready var container = $VBoxContainer
+@onready var container_purchase_items = $VBoxContainer/PurchaseItems
+@onready var container_purchased_items = $VBoxContainer/PurchasedItems
 
 var singleton
 
 func _ready() -> void:
-    print("_ready")
-    if !Engine.has_singleton("IOSInAppPurchase"):
-        print("IOSInAppPurchase is not found")
-        return
+	print("_ready")
+	if !Engine.has_singleton("IOSInAppPurchase"):
+		print("IOSInAppPurchase is not found")
+		return
 
-    print("IOSInAppPurchase is found")
-    singleton = Engine.get_singleton("IOSInAppPurchase")
-    singleton.response.connect(_receive_response)
+	print("IOSInAppPurchase is found")
+	singleton = Engine.get_singleton("IOSInAppPurchase")
+	singleton.response.connect(_receive_response)
 
-    # var data = {
-    #     "message":"hello"
-    # }
-    # print(singleton.request("test", data))
+	# var data = {
+	#     "message":"hello"
+	# }
+	# print(singleton.request("test", data))
 
-    # print(singleton.request("dummy", {}))
+	# print(singleton.request("dummy", {}))
 
-    var product_data = {
-        "product_ids":[
-            "dummy_consumable001", 
-            "dummy_non_consumable001"
-            ]
-        }
-    print(singleton.request("products", product_data))
+	var product_data = {
+		"product_ids":[
+			"dummy_consumable001", 
+			"dummy_non_consumable001"
+			]
+		}
+	print(singleton.request("products", product_data))
+	print(singleton.request("purchasedProducts", {}))
 
 func _receive_response(response_name:String, data:Dictionary) -> void:
-    print("response:%s data:%s" % [response_name, data])
-    match response_name:
-        "products":
-            call_deferred("list_products", data)
-        "purchase":
-            pass
+	print("response:%s data:%s" % [response_name, data])
+	match response_name:
+		"products":
+			call_deferred("update_purchase_items", data)
+		"purchase":
+			print(singleton.request("purchasedProducts", {}))
+		"purchasedProducts":
+			call_deferred("update_purchased_items", data)
 
-func list_products(data) -> void:
-    for child in container.get_children():
-        container.remove_child(child)
+func update_purchase_items(data) -> void:
+	for child in container_purchase_items.get_children():
+		container_purchase_items.remove_child(child)
 
-    var products = data["products"]
-    for product in products:
-        var button = ItemButton.instantiate()
-        container.add_child(button)
-        button.text = "ITEM:%s PRICE:%s TYPE:%s" % [
-            product["displayName"], 
-            product["displayPrice"],
-            product["type"]
-        ]
-        button.pressed.connect(purchase_item.bind(product["product_id"]))
+	var products = data["products"]
+	for product in products:
+		var button = ItemButton.instantiate()
+		container_purchase_items.add_child(button)
+		button.text = "ITEM:%s PRICE:%s TYPE:%s" % [
+			product["displayName"], 
+			product["displayPrice"],
+			product["type"]
+		]
+		button.pressed.connect(purchase_item.bind(product["product_id"]))
 
 func purchase_item(product_id) -> void:
-    print("purchase_item:%s" % product_id)
+	print("purchase_item:%s" % product_id)
+	singleton.request("purchase", {"product_id":product_id})
+
+func update_purchased_items(data) -> void:
+	for child in container_purchased_items.get_children():
+		container_purchased_items.remove_child(child)
+	var product_ids = data["product_ids"]
+	for product_id in product_ids:
+		var label = ItemLabel.instantiate()
+		container_purchased_items.add_child(label)
+		label.text = product_id
