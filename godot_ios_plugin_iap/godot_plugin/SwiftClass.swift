f784f3f7b53f7afbee59ac610e64c0f348b6be4a
Hiroki Taira <58840595+hrk4649@users.noreply.github.com>
2025-04-20 06:17:19 +0900

    use JSONSerialization.jsonObject()

--- godot_ios_plugin_iap/godot_plugin/SwiftClass.swift
+++ godot_ios_plugin_iap/godot_plugin/SwiftClass.swift
@@ -213,50 +213,15 @@ import StoreKit
     }
     
     static func convertEntitlement(transaction:Transaction, error:Error?) ->[String:Any] {
-        var entitlement = [
-            "id":transaction.id,
-            "originalID":transaction.originalID,
-            "webOrderLineItemID":transaction.webOrderLineItemID ?? "",
-            "productId":transaction.productID,
-            "subscriptionGroupID":transaction.subscriptionGroupID ?? "",
-            "purchaseDate":dateToString(transaction.purchaseDate),
-            "originalPurchaseDate":dateToString(transaction.purchaseDate),
-            "expirationDate":dateToString(transaction.expirationDate),
-            "purchasedQuantity":transaction.purchasedQuantity,
-            "isUpgraded":transaction.isUpgraded,
-            // offer 17.2
-            //   vs
-            // offerType,
-            // offerID,
-            // offerPaymentModeStringRepresentation
-            // offerPeriodStringRepresentation
-            "revocationDate":dateToString(transaction.revocationDate),
-            "revocationReason":transaction.revocationReason?.rawValue ?? "",
-            "productType":transaction.productType,
-            "appAccountToken":transaction.appAccountToken ?? "",
-            // environment 16.0
-            //   vs
-            // environmentStringRepresentation
-            // reason 17.0
-            //   vs
-            // reasonStringRepresentation
-            // storefront 17.0
-            //   vs
-            // storefrontCountryCode
-            // price 15.0
-            // currency 16.0
-            //   vs
-            // currencyCode
-            "appTransactionID":transaction.appTransactionID,
-            // deviceVerification
-            // deviceVerificationNonce
-            "ownershipType":transaction.ownershipType.rawValue,
-            "signedDate":dateToString(transaction.signedDate),
-            // advancedCommerceInfo 18.4
-        ] as [String : Any]
+        let json = try? JSONSerialization.jsonObject(
+            with: transaction.jsonRepresentation, options: [])
+        
+        var entitlement : [String:Any] = json as? [String:Any] ?? [:]
+        
         if error != nil {
             entitlement["error"] = error!.localizedDescription
         }
+        
         return entitlement
     }
     
